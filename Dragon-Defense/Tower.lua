7a3ad1fbbc8ef5dabffa8fd5bf726556430eb4e7
Lars Mattsson <saberlost@gmail.com>
2012-01-10 03:00:05 +0100

    pre alpha 0.1

---
+++ Dragon-Defense/Tower.lua
@@ -0,0 +1,13 @@
+
+
+function NewTower(params)
+	local Tower  = display.newGroup()
+	if params.default then
+		default = display.newImage( params.default )
+		tower:insert( default, true )
+	end
+	
+	
+return Tower
+
+
